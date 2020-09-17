da406de89983488ac59f71cf441a22b2118ef242
VictorPonce27 <A00827302@itesm.mx>
2020-09-17 12:54:43 -0500

    agregar comentarios para ser entender el codigo mejor

--- packman.py
+++ packman.py
@@ -14,8 +14,10 @@ ghosts = [
     [vector(100, -160), vector(-5, 0), "purple"],
 ]
 
-# game baord
-
+######### game baord#############
+#   los 0 son paredes bloqueadas#
+#   Los 1 son rutas             #
+#################################
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
@@ -45,24 +47,27 @@ def square(x, y):
     path.goto(x, y)
     path.down()
     path.begin_fill()
-
+    #dibuja el cuadrado con un for loop de 4!
     for count in range(4):
         path.forward(20)
         path.left(90)
 
     path.end_fill()
 
+# funcion para que todo se vea bien
 def offset(point):
     "Return offset of point in tiles."
+    #utiliza un floor para poder tener un minimo!
     x = (floor(point.x, 20) + 200) / 20
     y = (180 - floor(point.y, 20)) / 20
     index = int(x + y * 20)
     return index
 
+#verificar que los fatnasmas y packman esten en una posicion valida!
 def valid(point):
     "Return True if point is valid in tiles."
     index = offset(point)
-
+    #verificar que el index del personaje no sea el 0 verificar con la linea 21 de tiles!!
     if tiles[index] == 0:
         return False
 
@@ -77,10 +82,10 @@ def world():
     "Draw world using path."
     bgcolor('black')
     path.color('blue')
-
+    #dibuja todo el mapa con un for loop de los tiles, ver linea 21  de tiles!!
     for index in range(len(tiles)):
         tile = tiles[index]
-
+        #los valores de 1 son el mapa que puede navegar el usuraio y los fantasmas!!
         if tile > 0:
             x = (index % 20) * 20 - 200
             y = 180 - (index // 20) * 20
@@ -90,7 +95,7 @@ def world():
                 path.up()
                 path.goto(x + 10, y + 10)
                 path.dot(2, 'white')
-
+#El movimiento de jugador y los fantasmas
 def move():
     "Move pacman and all ghosts."
     writer.undo()
@@ -98,12 +103,12 @@ def move():
 
     clear()
 
-    #packmans movemen
+    #movimiento del packamn
     if valid(pacman + aim):
         pacman.move(aim)
 
     index = offset(pacman)
-
+    # revisar que el packman tenga un punto al comer las bolitas blancas!!
     if tiles[index] == 1:
         tiles[index] = 2
         state['score'] += 1
@@ -112,16 +117,21 @@ def move():
         square(x, y)
 
     up()
+    #para que el packamn este sentrado!!
     goto(pacman.x + 10, pacman.y + 10)
     dot(20, 'yellow')
 
 
-# Movements of Ghosts
+# Movimiento de los fantasmas
     for point, course, color in ghosts:
         if valid(point + course):
             point.move(course)
         else:
-
+            #######################################################################################################################################
+            #verifica posicion del packman y hace que los fantasmas se muevan hacia el dependiendo del cuadrante que se encuentra el packman!!    #
+            #utilizando if statemnts y elif para poder validar esto!!                                                                             #
+            #verificando las cordenadas de pacman en 'x' y en 'y'                                                                                 #
+            #######################################################################################################################################
             if pacman.x > point.x  and pacman.y > point.y:
                 options = [
                     vector(0,5),
@@ -186,9 +196,10 @@ def move():
             return
 
     ontimer(move, 100)
-
+#verificar que la dirreccion del packman puede ser valid!!
 def change(x, y):
     "Change pacman aim if valid."
+    #if statemnts para poder ver que si puede estar en esa posicion el packman!!
     if valid(pacman + vector(x, y)):
         aim.x = x
         aim.y = y
